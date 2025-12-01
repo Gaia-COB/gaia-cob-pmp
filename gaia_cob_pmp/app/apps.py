4efe33641320e0edf06398d04acc3cad9bb9491e
Jamie Court <jc2a23@soton.ac.uk>
2025-12-01 16:45:18 +0000

    Register remaining models for table display with iommi app

--- gaia_cob_pmp/app/apps.py
+++ gaia_cob_pmp/app/apps.py
@@ -18,20 +18,32 @@ class MainAppConfig(AppConfig):
 
         from app.models import (
             Instrument,
+            Project,
+            Proposal,
+            Observation,
             Researcher,
             Source,
         )
 
         # Iommi path decoding and settings for model searches
         register_search_fields(
-            model=Source, search_fields=["name", "other_names"], allow_non_unique=True
-        )
-        register_path_decoding(source=Source)
-
-        register_search_fields(
             model=Instrument, search_fields=["name", "observatory"], allow_non_unique=True
         )
         register_path_decoding(instrument=Instrument)
+
+        register_path_decoding(observation=Observation)
+
+        register_search_fields(
+            model=Project, search_fields=["project_name", "principle_investigator__user__username"], allow_non_unique=True
+        )
+        register_path_decoding(project=Project)
+
+        register_path_decoding(proposal=Proposal)
+
+        register_search_fields(
+            model=Source, search_fields=["name", "other_names"], allow_non_unique=True
+        )
+        register_path_decoding(source=Source)
 
         register_search_fields(
             model=Researcher, search_fields=["user", "affiliations"], allow_non_unique=True
